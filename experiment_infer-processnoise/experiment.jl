8447338d52ce32a31b7b7473e477b216ffe9aba7
wmkouw <wmkouw@gmail.com>
2019-10-15 19:36:19 -0400

    Finished implementation of process noise estimation via equality node. Parameter seems to be under-estimated.

--- experiment_infer-processnoise/experiment.jl
+++ experiment_infer-processnoise/experiment.jl
@@ -26,12 +26,13 @@ include("../util.jl")
 # Data
 include("../gen_data.jl")
 
+
 """
 Experiment parameters
 """
 
 # Signal time horizon
-T = 80
+T = 1000
 
 # Reaction-time clock
 TT = 10
@@ -41,7 +42,7 @@ gain = 1.0
 emission = 1.0
 
 # Noise parameters
-process_noise = 1.2
+process_noise = 3.
 measurement_noise = 0.3
 
 # Clamped parameters
@@ -66,76 +67,69 @@ p(y_{1:T}, x_{0:T} | u_{1:T}) = p(x_0) Π_t p(y_t, x_t | x_{t-1})
 In other words, Markov chains of time-slices of a state-space models.
 Below, we specify the following model through the time-slice subgraph
 
-     γ′_x     e_γ
-...   ∘------>[=]                     ...
-               |
-               ∘ γ_x
-               |
-     x_t-1     |     x'_t    e_x
-...   ∘----->[g_t]----∘------[=]      ...
-                              |
-                              ∘ x_t
-                              |
-                            [f_t]
-                              |
-                              ⊡
-                             y_t
+                e_γ
+... (γ′_x)----->[=]----->(γ''_x)   ...
+                 |
+               (γ_x)
+                 |
+                _|_       ___
+... (x_t-1)--->[g_t]---->(x_t)     ...
+                          _|_
+                         [f_t]
+                           |
+                           ⊡
+                          y_t
 
 x_t-1 = previous state edge
 g_t = state transition edge
 γ_x = process noise edge
 γ'_x = previous process noise
+γ''_x = next process noise
 e_γ = process noise equality node
 x_t = current state edge
-x'_t = current state edge
-e_x = current state equality node
 f_t = likelihood node
 y_t = observation node
 """
 
 # Start graph
-graph = MetaGraph(SimpleGraph(10))
+graph = MetaGraph(SimpleGraph(9))
 
 # Edge variable: previous state
-set_props!(graph, 1, Dict{Symbol,Any}(:object => :x_tmin, :id => "x_tmin"))
+set_props!(graph, 1, Dict{Symbol,Any}(:object => :x_tmin, :id => "x_tmin", :type => "variable"))
 
 # Factor node: state transition
-set_props!(graph, 2, Dict{Symbol,Any}(:object => :g_t, :id => "g_t"))
+set_props!(graph, 2, Dict{Symbol,Any}(:object => :g_t, :id => "g_t", :type => "factor"))
 
 # Edge variable: current process noise
-set_props!(graph, 3, Dict{Symbol,Any}(:object => :γ_x, :id => "γ_x"))
+set_props!(graph, 3, Dict{Symbol,Any}(:object => :γ_x, :id => "γ_x", :type => "variable"))
 
 # Edge variable: previous process noise
-set_props!(graph, 4, Dict{Symbol,Any}(:object => :γ_xp, :id => "γ_xp"))
+set_props!(graph, 4, Dict{Symbol,Any}(:object => :γ_xp, :id => "γ_xp", :type => "variable"))
+
+# Edge variable: previous process noise
+set_props!(graph, 5, Dict{Symbol,Any}(:object => :γ_xpp, :id => "γ_xpp", :type => "variable"))
 
 # Factor node: process noise equality
-set_props!(graph, 5, Dict{Symbol,Any}(:object => :e_γ, :id => "e_γ"))
+set_props!(graph, 6, Dict{Symbol,Any}(:object => :e_γ, :id => "e_γ", :type => "factor"))
 
 # Edge variable: current state
-set_props!(graph, 6, Dict{Symbol,Any}(:object => :x_t, :id => "x_t"))
-
-# Factor node: current state equality
-set_props!(graph, 7, Dict{Symbol,Any}(:object => :e_x, :id => "e_x"))
-
-# Edge variable: current state
-set_props!(graph, 8, Dict{Symbol,Any}(:object => :x_tp, :id => "x_tp"))
+set_props!(graph, 7, Dict{Symbol,Any}(:object => :x_t, :id => "x_t", :type => "variable"))
 
 # Factor node: likelihood
-set_props!(graph, 9, Dict{Symbol,Any}(:object => :f_t, :id => "f_t"))
+set_props!(graph, 8, Dict{Symbol,Any}(:object => :f_t, :id => "f_t", :type => "factor"))
 
 # Edge variable: observation
-set_props!(graph, 10, Dict{Symbol,Any}(:object => :y_t, :id => "y_t"))
+set_props!(graph, 9, Dict{Symbol,Any}(:object => :y_t, :id => "y_t", :type => "variable"))
 
 # Add edges between factor nodes and variables
 add_edge!(graph, 1, 2) # x_t-1 -- g_t
 add_edge!(graph, 2, 3) # g_t -- γ_x
-add_edge!(graph, 3, 5) # γ_x -- e_γ
-add_edge!(graph, 4, 5) # γ_xp -- e_γ
-add_edge!(graph, 2, 6) # g_t -- x_t
-add_edge!(graph, 6, 7) # x_t -- e_x
-add_edge!(graph, 7, 8) # e_x --- x_tp
-add_edge!(graph, 8, 9) # x_tp -- f_t
-add_edge!(graph, 9, 10) # f_t -- y_t
+add_edge!(graph, 3, 6) # γ_x -- e_γ
+add_edge!(graph, 4, 6) # γ_xp -- e_γ
+add_edge!(graph, 5, 6) # γ_xpp -- e_γ
+add_edge!(graph, 2, 7) # g_t -- x_t
+add_edge!(graph, 7, 8) # x_t -- f_t
+add_edge!(graph, 8, 9) # f_t -- y_t
 
 # Ensure vertices can be recalled from given id
 set_indexing_prop!(graph, :id)
@@ -155,8 +149,7 @@ check = zeros(T, TT)
 global x_t = EdgeGaussian("x_0"; mean=x_0[1], precision=x_0[2])
 global γ_x = EdgeGamma("γ_0"; shape=γ_0[1], scale=γ_0[2])
 
-# for t = 1:T
-    t=1
+for t = 1:T
 
       # Report progress
       if mod(t, T/10) == 1
@@ -164,7 +157,7 @@ global γ_x = EdgeGamma("γ_0"; shape=γ_0[1], scale=γ_0[2])
       end
 
       # Previous state
-      global x_tmin = EdgeGaussian("x_tmin", mean=x_t.mean, precision=x_t.precision)
+      global x_tmin = EdgeGaussian("x_tmin", mean=x_t.mean, precision=x_t.precision, block=true)
 
       # Previous previous noise
       global γ_xp = EdgeGamma("γ_xp", shape=γ_x.shape, scale=γ_x.scale, block=true)
@@ -173,19 +166,16 @@ global γ_x = EdgeGamma("γ_0"; shape=γ_0[1], scale=γ_0[2])
       global g_t = TransitionGaussian("g_t", edge_mean="x_tmin", edge_data="x_t", edge_precision="γ_x")
 
       # Current process noise
-      global γ_x = EdgeGamma("γ_x", shape=1.0, scale=1.0)
+      global γ_x = EdgeGamma("γ_x", shape=γ_xp.shape, scale=γ_xp.scale)
 
       # Process noise equality node
-      global e_γ = NodeEquality("e_γ", edges=["γ_x", "γ_xp"])
+      global e_γ = NodeEquality("e_γ", edges=["γ_x", "γ_xp", "γ_xpp"])
+
+      # Next process noise
+      global γ_xpp = EdgeGamma("γ_xpp", shape=γ_xp.shape, scale=γ_xp.scale, silent=true)
 
       # Current state
-      global x_t = EdgeGaussian("x_t", mean=0.0, precision=1.0)
-
-      # State equality node
-      global e_x = NodeEquality("e_x", edges=["x_t", "x_tp"])
-
-      # Current state
-      global x_tp = EdgeGaussian("x_tp", mean=0.0, precision=1.0)
+      global x_t = EdgeGaussian("x_t", mean=x_tmin.mean, precision=x_tmin.precision)
 
       # Observation likelihood node
       global f_t = LikelihoodGaussian("f_t", edge_mean="x_t", edge_data="y_t", edge_precision=inv(measurement_noise))
@@ -203,23 +193,22 @@ global γ_x = EdgeGamma("γ_0"; shape=γ_0[1], scale=γ_0[2])
           # Write out estimated state parameters
           estimated_states[t, 1, tt] = x_t.mean
           estimated_states[t, 2, tt] = sqrt(1/x_t.precision)
-          estimated_noises[t, 1, tt] = γ_x.shape * γ_x.scale
-          estimated_noises[t, 2, tt] = sqrt(γ_x.shape * γ_x.scale^2)
-          check[t,tt] = γ_xp.shape * γ_xp.scale
+          estimated_noises[t, 1, tt] = γ_xpp.shape * γ_xpp.scale
+          estimated_noises[t, 2, tt] = sqrt(γ_xpp.shape * γ_xpp.scale^2)
 
-          # Keep track of FE gradients
-          free_energy_gradients[t, tt] = x_t.grad_free_energy
+          # # Keep track of FE gradients
+          # free_energy_gradients[t, tt] = x_t.grad_free_energy
 
           react(g_t, graph)
           react(γ_x, graph)
           react(e_γ, graph)
+          react(γ_xp, graph)
+          react(γ_xpp, graph)
           react(x_t, graph)
-          react(e_x, graph)
-          react(x_tp, graph)
           react(f_t, graph)
           react(y_t, graph)
       end
-# end
+end
 
 """
 Visualize experimental results
@@ -269,7 +258,7 @@ title!("Parameter trajectory of q(x_t) for t="*string(t))
 savefig(pwd()*"/experiment_infer-processnoise/viz/state_parameter_trajectory_t" * string(t) * ".png")
 
 # Visualize noise belief parameter trajectory
-t = 1
+t = T
 plot(estimated_noises[t,1,1:end], color="blue", label="q(γ_"*string(t)*")")
 plot!(estimated_noises[t,1,1:end],
       ribbon=[estimated_noises[t,2,1:end], estimated_noises[t,2,1:end]],
